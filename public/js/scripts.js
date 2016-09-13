5bd265d3f6d8cdd49efa21f377a367c76fc27f11
Devansh Kukreja <devu@MACDEVANSH.WV.CC.CMU.EDU>
2016-09-13 12:24:48 -0400

    fixed button feedback

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,10 +1,23 @@
 
 var ngrokUrl = "085eac96"
+var onBtn, offBtn
 
-function lightsOn(){
-  $.get("https://"+ngrokUrl+".ngrok.io/on");
+function lightsOn(btn){
+  // $.get("https://"+ngrokUrl+".ngrok.io/on");
+  clearTimeout($.data(this, 'onBtnTimer'));
+  onBtn = btn;
+  $(btn).addClass("active");
+  $.data(this, 'onBtnTimer', setTimeout(function() {
+    $(onBtn).removeClass("active");
+  }, 300));
 }
 
-function lightsOff(){
-  $.get("https://"+ngrokUrl+".ngrok.io/off");
+function lightsOff(btn){
+  // $.get("https://"+ngrokUrl+".ngrok.io/off");
+  clearTimeout($.data(this, 'offBtnTimer'));
+  offBtn = btn;
+  $(btn).addClass("active");
+  $.data(this, 'offBtnTimer', setTimeout(function() {
+    $(offBtn).removeClass("active");
+  }, 300));
 }
